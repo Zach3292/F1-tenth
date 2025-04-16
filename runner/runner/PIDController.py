ad7a7ad2c81dfc774ab9bddd097e7d56a6eca225
Zach3292 <zach3292@hotmail.com>
2025-04-16 16:18:22 -0400

    adapted for autodrive sim

---
+++ runner/runner/PIDController.py
@@ -0,0 +1,18 @@
+class PIDController:
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.prev_error = 0
+        self.integral = 0
+
+    def update(self, setpoint, measured_value, dt):
+        error = setpoint - measured_value
+        self.integral += error * dt
+        derivative = (error - self.prev_error) / dt
+
+        output = (self.kp * error) + (self.ki * self.integral) + (self.kd * derivative)
+
+        self.prev_error = error
+
+        return output
--- runner/runner/__init__.py
+++ runner/runner/__init__.py
@@ -1,41 +1,42 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String, Float32
+from sensor_msgs.msg import LaserScan, Imu
 from nav_msgs.msg import Odometry
 from ackermann_msgs.msg import AckermannDriveStamped
 import numpy as np
+from runner.PIDController import PIDController as PID
+
 
 class Gap_Finder(Node):
 
     ranges = np.array([])
     previous_time, time = 0, 0
     velocity = 0
-    kP, kI, kD = 0.75, 0, 1.25
-    previous_error, error = 0, 0
     setpoint = 0
-    P, I, D = 0, 0, 0
 
     angle_increment = 0
-    max_speed = 40/3.6
+    range_max = 0
 
     def __init__(self):
-        super().__init__('runner_node')
-        self.publisher_ = self.create_publisher(AckermannDriveStamped, '/drive', 10)
-        self.subscription1 = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
-        self.subscription2 = self.create_subscription(Odometry, '/ego_racecar/odom', self.odom_callback, 10)
-        self.get_logger().info('Runner node started')
+        super().__init__("runner_node")
+        # self.publisher1_ = self.create_publisher(AckermannDriveStamped, "/drive", 10)
+        self.publisher2_ = self.create_publisher(Float32, "/autodrive/f1tenth_1/steering_command", 10)
+        self.publisher3_ = self.create_publisher(Float32, "/autodrive/f1tenth_1/throttle_command", 10)
+        self.subscription1 = self.create_subscription(LaserScan, "/autodrive/f1tenth_1/lidar", self.scan_callback, 10)
+        self.subscription3 = self.create_subscription(Float32, "/autodrive/f1_tenth_1/speed", self.speed_callback, 10)
+        self.get_logger().info("Runner node started")
         # self.timer_ = self.create_timer(0.001, self.timer_callback)
-    
+
     def create_safety_bubble(self, ranges, threshold):
 
-        radius = 0.1
+        radius = 0.25
         for i in range(len(ranges)):
             if ranges[i] == np.nan:
                 ranges[i] = 0
             if ranges[i] < threshold and ranges[i] > 0:
-                bubble_index = int(2 * np.arcsin(radius/(2*ranges[i])) / self.angle_increment)
-                for i in range(i-bubble_index, i+bubble_index):
+                bubble_index = int(2 * np.arcsin(radius / (2 * ranges[i])) / self.angle_increment)
+                for i in range(i - bubble_index, i + bubble_index):
                     if i >= 0 and i < len(ranges):
                         ranges[i] = 0
 
@@ -59,7 +60,7 @@ class Gap_Finder(Node):
                         big_gap_start = gap_start
                         big_gap_end = gap_end
                     gap_start = np.inf
-        
+
         gap = [big_gap_start, big_gap_end]
         return gap
 
@@ -67,7 +68,7 @@ class Gap_Finder(Node):
         for i in range(gap[0], gap[1]):
             if ranges[i] > threshold:
                 ranges[i] = threshold
-        
+
         deep_gap = []
         gap_start = np.inf
         gap_end = 0
@@ -89,15 +90,15 @@ class Gap_Finder(Node):
             big_gap_end = gap[1]
         if big_gap_start == 0:
             big_gap_start = gap[0]
-        
+
         deep_gap = [big_gap_start, big_gap_end]
-        
+
         return int((deep_gap[1] + deep_gap[0]) / 2)
 
-
     def scan_callback(self, msg):
-        self.ranges = msg.ranges
+        self.ranges = np.copy(msg.ranges)
         self.angle_increment = msg.angle_increment
+        self.range_max = msg.range_max
         # angle_b = np.pi/2
         # reject_index = int((msg.angle_max-angle_b) / msg.angle_increment)
 
@@ -105,38 +106,43 @@ class Gap_Finder(Node):
         #     self.ranges[i] = 0
         # for i in range((len(self.ranges) - reject_index), len(self.ranges)):
         #     self.ranges[i] = 0
-        
+
         ranges_with_bubble = self.create_safety_bubble(self.ranges, 2)
         gap = self.find_largest_gap(ranges_with_bubble)
         center = self.center_of_deepest_gap(gap, ranges_with_bubble, 2.7)
-        angle = (center - len(msg.ranges)/2) * msg.angle_increment
-
+        angle = (center - len(self.ranges) / 2) * msg.angle_increment
 
         self.time = self.get_clock().now().nanoseconds / 1e6
 
-        error = self.setpoint - angle
-        self.P = self.kP * error
-        self.I += self.kI * (error * (self.time - self.previous_time))
-        self.D = self.kD * (error - self.previous_error) / (self.time - self.previous_time)
-        control = self.P + self.I + self.D
+        steering_PID = PID(1.1, 0.0001, 1.5)
+        throttle_PID = PID(0.05, 0.01, 0.01)
 
-        msg = AckermannDriveStamped()
+        steering = steering_PID.update(self.setpoint, angle, self.time - self.previous_time)
 
-        # if np.abs(control) >= 0 and np.abs(control) < 0.2:
-        #     msg.drive.speed = 1.5
-        # elif np.abs(control) >= 0.2 and np.abs(control) < 0.4:
-        #     msg.drive.speed = 1.0
-        # else:
-        #     msg.drive.speed = 0.5
+        steering_index = int((len(self.ranges) / 2) - (angle / self.angle_increment))
 
-        speed = (self.ranges[int(len(self.ranges)/2)] / 4)**2 * self.max_speed
+        steering_index = min(len(self.ranges) - 1, steering_index)
 
-        msg.drive.speed = speed
-        
-        msg.drive.steering_angle = -control
-        self.publisher_.publish(msg)
+        if msg.ranges[steering_index] == np.nan:
+            speed = 0
+        else:
+            speed = (msg.ranges[steering_index] / 4) * (np.abs(1 / steering) ** 3)
 
-        self.previous_error = error
+        throttle = throttle_PID.update(speed, self.velocity, self.time - self.previous_time)
+        throttle = min(1.0, throttle)
+        # msg = AckermannDriveStamped()
+        steering_msg = Float32()
+        throttle_msg = Float32()
+
+        throttle_msg.data = throttle
+        # msg.drive.speed = speed
+
+        # msg.drive.steering_angle = -control
+        steering_msg.data = -steering
+        # self.publisher1_.publish(msg)
+        self.publisher2_.publish(steering_msg)
+        self.publisher3_.publish(throttle_msg)
+
         self.previous_time = self.time
 
         # angle_b = np.pi/2
@@ -153,9 +159,8 @@ class Gap_Finder(Node):
 
         # self.future_measurement = measured_distance + self.velocity*(self.time - self.previous_time)*np.sin(alpha)
 
-
-    def odom_callback(self, msg):
-        self.velocity = msg.twist.twist.linear.x
+    def speed_callback(self, msg):
+        self.velocity = msg.data
 
 
 def main(args=None):
@@ -165,5 +170,6 @@ def main(args=None):
     runner_node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
