f414e5bf5e0a47a71a3b1a9ce98e9b1b02fbbf88
Zach3292 <zach3292@hotmail.com>
2025-04-21 20:51:17 -0400

    ekf runs not working

--- ekf/ekf/__init__.py
+++ ekf/ekf/__init__.py
@@ -1,8 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
+from geometry_msgs.msg import Pose
 from sensor_msgs.msg import Imu, JointState
-from ekf.ekf_msg import State
 import numpy as np
 
 
@@ -10,23 +10,25 @@ class Extended_Kalman_Filter(Node):
 
     angular_velocity = 0
     angular_velocity_covariance = 0
-    linear_acceleration = np.zeros((2, 1))
-    linear_acceleration_covariance = np.zeros((2, 2))
-    left_encoder_position = np.zeros((3, 1))
-    left_encoder_velocity = np.zeros((3, 1))
-    right_encoder_position = np.zeros((3, 1))
-    right_encoder_velocity = np.zeros((3, 1))
+    linear_acceleration = np.zeros(2, np.float32)
+    linear_acceleration_covariance = np.zeros((2, 2), np.float32)
+    left_encoder_position = 0
+    right_encoder_position = 0
+    previous_left_encoder_position = 0
+    previous_right_encoder_position = 0
+    left_encoder_velocity = 0
+    right_encoder_velocity = 0
 
-    stateMatrix = np.zeros((5, 1))
+    stateMatrix = np.zeros(5, np.float32)
 
     processCovariance = 0.025
-    processCovarianceMatrix = np.eye((5)) * processCovariance
+    processCovarianceMatrix = np.eye((5), dtype=np.float32) * processCovariance
 
     estimatedCovariance = 0.025
-    estimatedCovarianceMatrix = np.eye((5)) * estimatedCovariance
+    estimatedCovarianceMatrix = np.eye((5), dtype=np.float32) * estimatedCovariance
 
     encoderCovariance = 0.025
-    encoderCovarianceMatrix = np.eye((2)) * encoderCovariance
+    encoderCovarianceMatrix = np.eye((2), dtype=np.float32) * encoderCovariance
 
     delta_time = 1 / 60
 
@@ -35,7 +37,7 @@ class Extended_Kalman_Filter(Node):
 
     def __init__(self):
         super().__init__("ekf_node")
-        self.publisher_ = self.create_publisher(State, "/autodrive/f1tenth_1/state", 10)
+        self.publisher_ = self.create_publisher(Pose, "/autodrive/f1tenth_1/ekf_pose", 10)
         self.imu_subscription = self.create_subscription(Imu, "/autodrive/f1tenth_1/imu", self.imu_callback, 10)
         self.left_encoder_subscription = self.create_subscription(
             JointState, "/autodrive/f1tenth_1/left_encoder", self.left_encoder_callback, 10
@@ -57,21 +59,21 @@ class Extended_Kalman_Filter(Node):
         return
 
     def left_encoder_callback(self, msg):
-        self.left_encoder_position[0] = msg.position[0]
-        self.left_encoder_position[1] = msg.position[1]
-        self.left_encoder_position[2] = msg.position[2]
-        self.left_encoder_velocity[0] = msg.velocity[0]
-        self.left_encoder_velocity[1] = msg.velocity[1]
-        self.left_encoder_velocity[2] = msg.velocity[2]
+        self.previous_left_encoder_position = self.left_encoder_position
+        self.left_encoder_position = msg.position[0]
+        self.left_encoder_velocity = (self.left_encoder_position - self.previous_left_encoder_position) / (
+            msg.header.stamp.sec
+        )
+        self.left_encoder_velocity = self.left_encoder_velocity / self.wheel_radius
         return
 
     def right_encoder_callback(self, msg):
-        self.right_encoder_position[0] = msg.position[0]
-        self.right_encoder_position[1] = msg.position[1]
-        self.right_encoder_position[2] = msg.position[2]
-        self.right_encoder_velocity[0] = msg.velocity[0]
-        self.right_encoder_velocity[1] = msg.velocity[1]
-        self.right_encoder_velocity[2] = msg.velocity[2]
+        self.previous_right_encoder_position = self.right_encoder_position
+        self.right_encoder_position = msg.position[0]
+        self.right_encoder_velocity = (self.right_encoder_position - self.previous_right_encoder_position) / (
+            msg.header.stamp.sec
+        )
+        self.right_encoder_velocity = self.right_encoder_velocity / self.wheel_radius
         return
 
     def main_loop(self):
@@ -83,7 +85,8 @@ class Extended_Kalman_Filter(Node):
                 [0, 0, 1, 0, 0],
                 [0, 0, 0, 1, 0],
                 [0, 0, 0, 0, 1],
-            ]
+            ],
+            dtype=np.float32,
         )
 
         imu_control_matrix = np.array(
@@ -93,7 +96,8 @@ class Extended_Kalman_Filter(Node):
                 self.linear_acceleration[0] * self.delta_time,
                 self.linear_acceleration[1] * self.delta_time,
                 self.angular_velocity * self.delta_time,
-            ]
+            ],
+            dtype=np.float32,
         )
 
         imu_covariance_1D = np.array(
@@ -102,54 +106,51 @@ class Extended_Kalman_Filter(Node):
                 self.linear_acceleration_covariance[1][1],
                 self.linear_acceleration_covariance[0][0],
                 self.linear_acceleration_covariance[1][1],
-                self.angular_velocity_covariance
-            ]
+                self.angular_velocity_covariance,
+            ],
+            dtype=np.float32,
         )
 
         imu_covariance_matrix = np.dot(imu_covariance_1D, imu_covariance_1D.T)
 
-        imu_estimated_state = (
-            np.dot(A_matrix, self.stateMatrix)
-            + imu_control_matrix
-            + imu_covariance_1D
-        )
-        
-        va = (self.left_encoder_velocity[0] + self.right_encoder_velocity[0]) / 2
-        
+        imu_estimated_state = np.dot(A_matrix, self.stateMatrix) + imu_control_matrix + imu_covariance_1D
+
+        va = (self.left_encoder_velocity + self.right_encoder_velocity) / 2
+
         encoder_control_matrix = np.array(
             [
-                va * np.cos(self.stateMatrix[4]) * self.delta_time, 
+                va * np.cos(self.stateMatrix[4]) * self.delta_time,
                 va * np.sin(self.stateMatrix[4]) * self.delta_time,
                 va * np.cos(self.stateMatrix[4]),
                 va * np.sin(self.stateMatrix[4]),
-                (self.right_encoder_velocity[0] - self.left_encoder_velocity[0]) * self.delta_time / self.wheel_base
-            ]
+                (self.right_encoder_velocity - self.left_encoder_velocity) * self.delta_time / self.wheel_base,
+            ],
+            dtype=np.float32,
         )
 
-        encoder_covariance_1D = (np.ones((5, 1)) * self.encoderCovariance)
+        encoder_covariance_1D = np.ones(5) * self.encoderCovariance
 
         encoder_covariance_matrix = np.dot(encoder_covariance_1D, encoder_covariance_1D.T)
 
-        encoder_estimated_state = (
-            np.dot(A_matrix, self.stateMatrix)
-            + encoder_control_matrix
-            + encoder_covariance_1D
-        )
+        encoder_estimated_state = np.dot(A_matrix, self.stateMatrix) + encoder_control_matrix + encoder_covariance_1D
 
         P = np.dot(np.dot(A_matrix, self.estimatedCovarianceMatrix), A_matrix.T) + imu_covariance_matrix
 
         K = np.dot(P, np.linalg.inv(P + encoder_covariance_matrix))
 
         self.stateMatrix = imu_estimated_state + np.dot(K, (encoder_estimated_state - imu_estimated_state))
+        self.get_logger().info(f"State Matrix: {self.stateMatrix}")
 
-        self.estimatedCovarianceMatrix = np.dot((np.eye(5) - K), P)
+        self.estimatedCovarianceMatrix = np.dot((np.eye(5, dtype=np.float32) - K), P)
 
-        state_msg = State()
-        state_msg.x = self.stateMatrix[0]
-        state_msg.y = self.stateMatrix[1]
-        state_msg.xdot = self.stateMatrix[2]
-        state_msg.ydot = self.stateMatrix[3]
-        state_msg.theta = self.stateMatrix[4]
+        state_msg = Pose()
+        state_msg.position.x = self.stateMatrix[0]
+        state_msg.position.y = self.stateMatrix[1]
+        state_msg.orientation.w = np.cos(self.stateMatrix[4] / 2)
+        state_msg.orientation.z = np.sin(self.stateMatrix[4] / 2)
+        state_msg.orientation.x = 0.0
+        state_msg.orientation.y = 0.0
+        state_msg.position.z = 0.0
 
         self.publisher_.publish(state_msg)
 
